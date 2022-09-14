9202f794917cb27ebf9705b1a2d00bac1f1e0323
YanPavlukovich <filin022@gmail.com>
2022-09-14 20:29:18 +0300

    Добавил класс TestFunc с параметром для степени.

---
+++ Сортировка критических пациентов/TestFunc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Сортировка_критических_пациентов
+{
+    internal class TestFunc
+    {
+        public int Power(int a, int p)
+        {
+            return (int)Math.Pow(a, p);
+        }
+    }
+}
