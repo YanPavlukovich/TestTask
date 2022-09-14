d8f9d5c2b1d10c4c4aa4a1922f3276678f3c60f6
YanPavlukovich <filin022@gmail.com>
2022-09-14 20:37:10 +0300

    Добавили функцию для степени.

---
+++ Сортировка критических пациентов/TestFunc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Сортировка_критических_пациентов
+{
+    internal class TestFunc
+    {
+        public static int PowerOf2(int a)
+        {
+            return a * a;
+        }
+    }
+}
